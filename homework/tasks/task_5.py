60f8a02d1ff4764a9f9a7a5640572b2d51291cd4
Islamkhan S <143206727+islamkhanx@users.noreply.github.com>
2023-12-28 20:12:28 +0500

    initial solutions

--- homework/tasks/task_5.py
+++ homework/tasks/task_5.py
@@ -3,17 +3,12 @@ from typing import Coroutine
 
 
 async def limit_execution_time(coro: Coroutine, max_execution_time: float) -> None:
-    # Функция принимает на вход корутину, которую необходимо запустить, однако иногда она выполняется
-    # слишком долго, это время необходимо ограничить переданным на вход количеством секунд.
-    #
-    # Тест проверяет, что каждая переданная корутина была запущена, и все они завершились за заданное
-    # время.
-    #
-    # YOUR CODE GOES HERE
+    try:
+        await asyncio.wait_for(coro, timeout=max_execution_time)
+    except asyncio.TimeoutError:
+        print('Execution time exceeded')
 
 
 async def limit_execution_time_many(*coros: Coroutine, max_execution_time: float) -> None:
-    # Функция эквивалентна limit_execution_time, но корутин на вход приходит несколько.
-    #
-    # YOUR CODE GOES HERE
-
+    tasks = [limit_execution_time(coro, max_execution_time) for coro in coros]
+    await asyncio.gather(*tasks)
